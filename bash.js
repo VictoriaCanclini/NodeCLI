fefd01a3a5125e66592e2f1ab34cefdb0ba16c0f
Victoria Canclini <vico_canclini@hotmail.com>
2023-07-18 15:44:33 -0300

    node.js

---
+++ bash.js
@@ -0,0 +1,12 @@
+process.stdout.write("prompt >");
+const commands = require("./commands");
+process.stdin.on("data", function (data) {
+  let cmd = data.toString().trim();
+  let [command, ...argumento] = cmd.split(" ");
+  if (commands[command]) {
+    commands[command](argumento);
+  } else {
+    process.stdout.write("you typed: " + cmd);
+    process.stdout.write("\nprompt >");
+  }
+});
